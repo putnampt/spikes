8694d083f96b305bffc9788e9d1af94adca48940
putnampt <putnampt@gmail.com>
2014-04-24 11:21:32 -0700

    Added and changed several non-functional functions

---
+++ boxcar.m
@@ -0,0 +1,7 @@
+function [ output_args ] = boxcar( input_args )
+%BOXCAR Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ halfgaussian.m
@@ -0,0 +1,7 @@
+function [ output_args ] = halfgaussconvolve( input_args )
+%HALFGAUSSCONVOLVE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
