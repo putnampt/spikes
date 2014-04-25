bf25854756bc5b4ba55827a3329802c481b7523a
putnampt <putnampt@gmail.com>
2014-04-25 10:52:07 -0700

    halfgaussian -> halfgaussconvolve; now functional

---
+++ halfgaussconvolve.m
@@ -0,0 +1,12 @@
+function [ estimatedRate ] = halfgaussconvolve( spikes, width )
+%HALFGAUSSCONVOLVE Summary of this function goes here
+%   Detailed explanation goes here
+output_args = 0;
+wholeWidth =width*2;
+alpha = 8;
+wholeGauss = gausswin(wholeWidth,alpha);
+halfGauss = wholeGauss((size(wholeGauss,1)/2):end);
+estimatedRate = conv(halfGauss,spikes);
+
+end
+
--- slidingwindow.m
+++ slidingwindow.m
@@ -1,1 +1,14 @@
-function slid
+function [estimatedRate] = slidingwindow(spikes, size, overlap)
+%SLIDINGWINDOW Summary of this function goes here
+%   Detailed explanation goes here
+
+    if unique(spikes) == [0, 1] % If spikes are represented in a logical vector
+        
+        estimatedRate = zeros(1, size(spikes,2)); % Preallocate _estimatedRate_
+ 
+    else % If spikes are represented in time points
+        
+    end
+    
+end
+
